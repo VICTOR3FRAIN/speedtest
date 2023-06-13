80cfa5a1b2be26a42c63380b062c755e7268dffb
VICTOR3FRAIN <39277188+VICTOR3FRAIN@users.noreply.github.com>
2023-06-13 00:13:59 -0600

    Update Invoke-SpeedTest.psm1

--- Invoke-SpeedTest/Invoke-SpeedTest.psm1
+++ Invoke-SpeedTest/Invoke-SpeedTest.psm1
@@ -8,17 +8,15 @@ Using REST API GET method (via Invoke-RestMethod) for parsing JSON report
 Works in PSVersion 5.1 (PowerShell 7.3 not supported)
 .DESCRIPTION
 Example:
-$SpeedTest = Invoke-SpeedTestConserva # Output to variable full report
-Invoke-SpeedTestConserva -LogWrite # Write to log
-Invoke-SpeedTestConserva -LogWrite -LogPath "$home\Documents\Conserva-SpeedTest-Log.txt" # Set default path for log
-Invoke-SpeedTestConserva -LogRead | ft # Out log to PSObject
-Invoke-SpeedTestConserva -LogClear # Clear log file
-.LINK
-https://github.com/Lifailon/Ookla-SpeedTest-API
+$SpeedTest = Invoke-SpeedTest # Output to variable full report
+Invoke-SpeedTest -LogWrite # Write to log
+Invoke-SpeedTest -LogWrite -LogPath "$home\Documents\Ookla-SpeedTest-Log.txt" # Set default path for log
+Invoke-SpeedTest -LogRead | ft # Out log to PSObject
+Invoke-SpeedTest -LogClear # Clear log file
 #>
 param(
     [switch]$LogWrite,
-    $LogPath = "$home\Documents\Conserva-SpeedTest-Log.txt",
+    $LogPath = "$home\Documents\SpeedTest-Log.txt",
     [switch]$LogRead,
     [switch]$LogClear
 )
@@ -29,83 +27,102 @@ if ($LogClear) {
 }
 
 if (!$LogRead) {
-$ie = New-Object -ComObject InternetExplorer.Application
-$ie.navigate("https://www.speedtest.net")
+    $ie = New-Object -ComObject InternetExplorer.Application
+    $ie.navigate("https://www.speedtest.net")
 
-while ($True) {
-    if ($ie.ReadyState -eq 4) {
-        break
-    } else {
-        sleep -Milliseconds 100
+    while ($True) {
+        if ($ie.ReadyState -eq 4) {
+            break
+        } else {
+            sleep -Milliseconds 100
+        }
     }
-}
 
-$SPAN_Elements = $ie.document.IHTMLDocument3_getElementsByTagName("SPAN")
-$Go_Button = $SPAN_Elements | ? innerText -like "go"
-$Go_Button.Click()
+    $SPAN_Elements = $ie.document.IHTMLDocument3_getElementsByTagName("SPAN")
+    $Go_Button = $SPAN_Elements | ? innerText -like "go"
+    $Go_Button.Click()
 
-### Get result URL
-$Source_URL = $ie.LocationURL
-$Sec = 0
-while ($True) {
-    if ($ie.LocationURL -notlike $Source_URL) {
-        Write-Progress -Activity "SpeedTest Completed" -PercentComplete 100
-        $Result_URL = $ie.LocationURL
-        $ie.Quit()
-        break
-    } else {
-        sleep 1
-        $Sec += 1
-        Write-Progress -Activity "Started SpeedTest" -Status "Run time: $Sec sec" -PercentComplete $Sec
+    ### Get result URL
+    $Source_URL = $ie.LocationURL
+    $Sec = 0
+    while ($True) {
+        if ($ie.LocationURL -notlike $Source_URL) {
+            Write-Progress -Activity "SpeedTest Completed" -PercentComplete 100
+            $Result_URL = $ie.LocationURL
+            $ie.Quit()
+            break
+        } else {
+            sleep 1
+            $Sec += 1
+            Write-Progress -Activity "Started SpeedTest" -Status "Run time: $Sec sec" -PercentComplete $Sec
+        }
     }
-}
 
-### Parsing Web Content (JSON)
-$Cont = irm $Result_URL
-$Data = ($Cont -split "window.OOKLA.")[3] -replace "(.+ = )|(;)" | ConvertFrom-Json
+    ### Parsing Web Content (JSON)
+    $Cont = irm $Result_URL
+    $Data = ($Cont -split "window.OOKLA.")[3] -replace "(.+ = )|(;)" | ConvertFrom-Json
 
-### Convert Unix Time
-$EpochTime = [DateTime]"1/1/1970"
-$TimeZone = Get-TimeZone
-$UTCTime = $EpochTime.AddSeconds($Data.result.date)
-$Data.result.date = $UTCTime.AddMinutes($TimeZone.BaseUtcOffset.TotalMinutes)
+    ### Convert Unix Time
+    $EpochTime = [DateTime]"1/1/1970"
+    $TimeZone = Get-TimeZone
+    $UTCTime = $EpochTime.AddSeconds($Data.result.date)
+    $Data.result.date = $UTCTime.AddMinutes($TimeZone.BaseUtcOffset.TotalMinutes)
 
-
-    $mysql_server = "187.188.165.208:6001"
-    $mysql_user = "consultores_speedtest "
-    $mysql_password = "EfcD2RzPvykzWfbAMiRddAjwQTFRK6MT2sr75fwC..."
-    $dbName = "speedtest"
-    [void][system.reflection.Assembly]::LoadFrom("C:\Program Files (x86)\MySQL\MySQL Connector NET 8.0.33\MySql.Data.dll")
-    $Connection = New-Object -TypeName MySql.Data.MySqlClient.MySqlConnection
-    $Connection.ConnectionString = "SERVER=$mysql_server;DATABASE=$dbName;UID=$mysql_user;PWD=$mysql_password"
-    $Connection.Open()
+    [void][System.Reflection.Assembly]::LoadWithPartialName("C:\Program Files (x86)\MySQL\MySQL Connector NET 8.0.332\MySql.Data")
+    $MySQLAdminUserName = 'consultores_speedtest'
+    $MySQLAdminPassword = 'EfcD2RzPvykzWfbAMiRddAjwQTFRK6MT2sr75fwC...'
+    $MySQLDatabase = 'speedtest'
+    $MySQLHost = '187.188.165.208'
+    $ConnectionString = "server=" + $MySQLHost + ";port=6001;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";database="+$MySQLDatabase
 
     ## inicio - inserta en archivo txt
         if ($LogWrite) {
-        $sql = New-Object MySql.Data.MySqlClient.MySqlCommand
-        $sql.Connection = $Connection
-        $sql.CommandText = "insert into reg_diario (id_cata_sucursal,connection_icon,download,upload,latency,distance,server_id,sponsor_name,isp_name,idle_latency,download_latency,upload_latency,fecha_consulta) VALUE (32,$Data.result.connection_icon,$Data.result.download,$Data.result.upload,$Data.result.latency,$Data.result.distance,$Data.result.server_id,$Data.result.sponsor_name,$Data.result.isp_name,$Data.result.idle_latency,$Data.result.download_latency,$Data.result.upload_latency,$Data.result.fecha_consulta)"
-        $sql.ExecuteNonQuery()
+        
+        id_cata_sucursal = 32
+        connection_icon = $Data.result.connection_icon
+        download = $Data.result.download
+        upload = $Data.result.upload
+        latency = $Data.result.latency
+        distance = $Data.result.distance
+        server_id = $Data.result.server_id
+        sponsor_name = $Data.result.sponsor_name
+        isp_name = $Data.result.isp_name
+        idle_latency = $Data.result.idle_latency
+        download_latency = $Data.result.download_latency
+        upload_latency = $Data.result.upload_latency
+        fecha_consulta = $Data.result.fecha_consulta
 
-$time = $Data.result.date
-$ping = $Data.result.idle_latency
+        $Query = "insert into reg_diario (id_cata_sucursal,connection_icon,download,upload,latency,distance,server_id,sponsor_name,isp_name,idle_latency,download_latency,upload_latency,fecha_consulta) VALUE ($id_cata_sucursal,$connection_icon,$download,$upload,$latency,$distance,$server_id,$sponsor_name,$isp_name,$idle_latency,$download_latency,$upload_latency,$fecha_consulta)"
+        
+        $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection
+        $Connection.ConnectionString = $ConnectionString
+        $Connection.Open()
+            $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query, $Connection)
+            $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($Command)
+            $DataSet = New-Object System.Data.DataSet
+            $RecordCount = $dataAdapter.Fill($dataSet, "data")
+            $DataSet.Tables[0]
+        $Connection.Close()
 
-$Download = [string]($Data.result.download)
-$d2 = $Download[-3..-1] -Join ""
-$d1 = $Download[-10..-4] -Join ""
-$down = "$d1.$d2 Mbit"
+        $time = $Data.result.date
+        $ping = $Data.result.idle_latency
 
-$Upload = [string]($Data.result.upload)
-$u2 = $Upload[-3..-1] -Join ""
-$u1 = $Upload[-10..-4] -Join ""
-$up = "$u1.$u2 Mbit"
+        $Download = [string]($Data.result.download)
+        $d2 = $Download[-3..-1] -Join ""
+        $d1 = $Download[-10..-4] -Join ""
+        $down = "$d1.$d2 Mbit"
 
-$Out_Log = "$time  Download: $down  Upload: $up  Ping latency: $ping ms"
-$Out_Log >> $LogPath
-}
-$Connection.Close()
+        $Upload = [string]($Data.result.upload)
+        $u2 = $Upload[-3..-1] -Join ""
+        $u1 = $Upload[-10..-4] -Join ""
+        $up = "$u1.$u2 Mbit"
 
-$Data.result
+        $Out_Log = "$time  Download: $down  Upload: $up  Ping latency: $ping ms"
+        $Out_Log >> $LogPath
+        }
+ 
+    ## fin insert archivo txt
+    $Data.result
 }
 
 if ($LogRead) {
